bb81877a2995925a421ea23a3a810c8c92a1d1ed
ckjets <ck_jets@yahoo.co.jp>
2020-02-23 13:38:46 +0900

    テキストフィールドコンポーネントをatomsに追加

---
+++ src/components/atoms/ PrefInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="6" md="3">
+      <v-text-field
+        label="都道府県を入力"
+        prepend-icon="place"
+      ></v-text-field>
+      </v-col>
+  </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
+
+export default Vue.extend({
+  name: 'PrefInput',
+
+  data: () => ({
+  })
+})
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <PrefInput/>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PrefInput from '@/components/atoms/ PrefInput.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    PrefInput
   }
 }
 </script>
